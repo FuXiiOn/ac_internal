06af4f34653029c41b113fa4653538c2d030f3c9
FuXiiOn <48155066+FuXiiOn@users.noreply.github.com>
2024-11-29 18:27:39 +0100

    yes

--- proc.h
+++ proc.h
@@ -2,7 +2,6 @@
 #include <vector>
 #include <windows.h>
 #include <TlHelp32.h>
-//https://guidedhacking.com/threads/how-to-hack-any-game-first-internal-hack-dll-tutorial.12142/
 
 DWORD GetProcId(const wchar_t* procName);
 
